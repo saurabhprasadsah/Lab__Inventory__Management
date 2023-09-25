423987f9ac10f68e6b3bbc3338563044c06b1aa1
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-25 12:15:48 +0530

    forms.py

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -7,13 +7,18 @@ app_name= "mainapp"
 urlpatterns = [
     path('',Login),
     path("upload",chain_of_custody_form, name="uploads"),
+
     path('homepage/', homePage),
-    path('chain_of_custody/', chain_of_custody, ),
+    path('chain_of_custody/', chain_of_custody),
     path('chain_of_custody_form/', chain_of_custody_form),
+
+
     path('edit/<int:id>/', editrecord),
     path('SampleIntakelog/', SampleIntakelog),
     path('editsampleform/<int:id>/',editsampleform),
     path('Sample_Intake_Form/', Sample_Intake_Form),
+
+    
     path('SampleTrackingLog/', SampleTrackingLog),
     path('SampleTrackingForm/', SampleTrackingForm),
     path('SampleRetainLog/', SampleRetainLog),
