6473357fd41597f62588f63f13a1e1382e8f7ed2
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-25 11:50:51 +0530

    forms.py

---
+++ mainApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import *
+
+class ChainOfCustody(forms.ModelForm):
+    class Meta: #meta will be represent the innner class Employeeform
+        model =ChainOfCustody
+        fields= ['Date','Client_ID','Order_of_the_Form','COC_File_Upload']
+        widgets= {
+            'Date':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter Date'}),
+            'Client_ID':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter Client id'}),
+            'Order_of_the_Form':forms.TextInput(attrs={'class':'forms.control','placeholder':'Enter order of the Number'}),
+            'COC_File_Upload':forms.TextInput(attrs={'class':'forms.control','placeholder':'uploads files'}),
+        }
